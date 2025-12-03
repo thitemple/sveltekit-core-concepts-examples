83d2b141fe389bf3c1db2551d002cefe47ce44a2
Thiago Temple <thitemple@icloud.com>
2025-12-03 10:47:45 -0500

    New trip form with valibot

--- remote-functions-form/src/routes/trips/new/+page.svelte
+++ remote-functions-form/src/routes/trips/new/+page.svelte
@@ -57,6 +57,11 @@
 					bind:value={startDate}
 					class="block w-full rounded border border-slate-300 px-3 py-2 text-sm"
 				/>
+				{#if form?.errors?.startDate}
+					<p class="mt-1 text-xs text-red-600">
+						{form.errors.startDate}
+					</p>
+				{/if}
 			</div>
 
 			<div class="space-y-1">
